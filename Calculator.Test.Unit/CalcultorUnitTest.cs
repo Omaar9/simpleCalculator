1007d54e61a4b70c76e90fed2c2a48734ca5e1f9
Omaar9 <farhaan9@hotmail.com>
2019-02-12 13:12:58 +0100

    correct test cases

--- Calculator.Test.Unit/CalcultorUnitTest.cs
+++ Calculator.Test.Unit/CalcultorUnitTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
@@ -15,7 +16,7 @@ namespace Calculator.Test.Unit
         [SetUp]
         public void Setup()
         {
-             uut = new simpleCalculator.Calculator();
+            uut = new simpleCalculator.Calculator();
         }
 
         [Test]
@@ -25,61 +26,86 @@ namespace Calculator.Test.Unit
             //Act
             var result = uut.Add(2, 4);
 
-            //Asert
+            //Assert
             Assert.That(result, Is.EqualTo(6));
-
-            //Eller
-            Assert.That(uut.Add(8, 2), Is.EqualTo(10));
-            Assert.That(uut.Add(16, 4), Is.EqualTo(20));
-
         }
 
-        [Test]
+        // TestCases
+        [TestCase(8, 2, 10)]
+        [TestCase(16, 4, 20)]
+        [TestCase(20, 5, 25)]
+
+        public void Add_AddPosAndNeg_Result()
+        {
+            //Act
+            var result = uut.Add(-2, 4);
+
+            //Assert 
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        // TestCases
+        [TestCase(-3, 9, 6)]
+        [TestCase(-4, 10, 6)]
+        [TestCase(12, -6, 6)]
+
         //Subtraktion
         public void Subtract_SubtractAandB_Result()
         {
             //Act
             var result = uut.Subtract(8, 4);
 
-            //Asert
+            //Assert
             Assert.That(result, Is.EqualTo(4));
-
-            //Eller
-            Assert.That(uut.Subtract(8, 4), Is.EqualTo(4));
-            Assert.That(uut.Subtract(10,5),Is.EqualTo(5));
-            Assert.That(uut.Subtract(20, 10), Is.EqualTo(10));
-
         }
 
-        [Test]
+        // TestCases
+        [TestCase(8, 4, 4)]
+        [TestCase(10, 5, 5)]
+        [TestCase(20, 10, 10)]
+
+        public void Subtract_SubtractPosAndNeg_Result()
+        {
+            //Act
+            var result = uut.Subtract(-2, 4);
+
+            //Assert 
+            Assert.That(result, Is.EqualTo(-6));
+        }
+
+        // TestCases
+        [TestCase(-3, 9, -12)]
+        [TestCase(-4, 10, -14)]
+        [TestCase(12, -6, 18)]
+
         //Multiplikation
-        public void Multiply_MultiplyAandB_Return64()
+        public void Multiply_MultiplyAandB_Return49()
         {
             //Act
             var result = uut.Multiply(7, 7);
 
-            //Asert
+            //Assert
             Assert.That(result, Is.EqualTo(49));
-
-            //Eller
-            Assert.That(uut.Multiply(7, 7), Is.EqualTo(49));
-            Assert.That(uut.Multiply(6, 4), Is.EqualTo(24));
-            Assert.That(uut.Multiply(8, 2), Is.EqualTo(16));
-
         }
 
-        [Test]
+        [TestCase(7, 7, 49)]
+        [TestCase(6, 4, 24)]
+        [TestCase(8, 2, 16)]
+
         public void Power_NumberAandB_Result()
         {
             //Act
-            var result = uut.Power(8, 8);
+            var result = uut.Power(8, 2);
 
             //Asert
-            Assert.That(result, Is.EqualTo(16777216));
+            Assert.That(result, Is.EqualTo(64));
 
             //Eller
-            Assert.That(uut.Power(8, 8), Is.EqualTo(16777216));
+            Assert.That(uut.Power(8, 2), Is.EqualTo(64));
         }
-
+        //[TestCase(8, 2, 64)]
+        //[TestCase(2, 3, 8)]
+        //[TestCase(1, 4, 1)]
+    
     }
 }
